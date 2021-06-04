8d675a441fbb771380d3a0260b6c9345507688d9
biko2020 <brahimlion38@hotmail.com>
2021-06-04 17:37:53 +0100

    add class Film with there entitis

--- src/main/java/dao/Film.java
+++ src/main/java/dao/Film.java
@@ -1,4 +1,28 @@
 package dao;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Date;
+
+@Entity @AllArgsConstructor @NoArgsConstructor @ToString
+@Data
+
 public class Film {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id_film;
+    private String nom_film;
+    private String realisateur;
+    private Date dateSortie_film;
+    private double durre_film;
+    private String photo_film;
+    @OneToMany(mappedBy = "film")
+    private Collection<Projection> projections;
+    @ManyToOne
+    private  Categorie categorie;
+
 }

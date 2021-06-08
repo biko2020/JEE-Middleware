2b56a681bc0f0cb517e2ac15811a5fb766a5b3f0
brahim <57845564+biko2020@users.noreply.github.com>
2021-06-08 10:43:36 +0100

    add entityscan for backage name

--- src/main/java/com/enset/cinema/CinemaApplication.java
+++ src/main/java/com/enset/cinema/CinemaApplication.java
@@ -2,8 +2,13 @@ package com.enset.cinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"com.enset.dao"})
+
 public class CinemaApplication {
 
     public static void main(String[] args) {
